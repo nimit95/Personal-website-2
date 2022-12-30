05c777c2f08296de2fd6336401288a544636ebed
nimit95 <nimitagg95@gmail.com>
2022-12-30 16:02:58 +0000

    sadding giscus

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' substack.com vitals.vercel-insights.com gitscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' substack.com vitals.vercel-insights.com giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
